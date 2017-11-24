0bedd76404327ccd6a4bcf21fa734a521f7973fa
tuvudu <tuyenvuduc@yahoo.com>
2017-11-24 20:48:31 +0700

    avatar upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :avatar, AvatarUploader
+  
   has_many :team_users
   has_many :teams, through: :team_users
   has_many :projects, foreign_key: :owner_id 
