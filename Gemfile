0ca79a00791666cde58f3537d35e12afd5b77496
ngoann <wapa2pro@gmail.com>
2017-11-25 11:32:09 +0700

    Admin

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ group :production do
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "rails_admin"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "projects#index"
   devise_for :users
   patch "edit/avatar", to: "users#update_avatar", as: "edit_avatar"
